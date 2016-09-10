a55682b0f2fe8aeca36450d98dcbf0f3ec3b3a79
canperk <canperk@outlook.com>
2016-09-10 14:05:30 +0300

    Resource files

---
+++ ECButton.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace EduCare.TeacherEye.Common.Controls
+{
+    public class ECButton : Button
+    {
+        public string IconText
+        {
+            get { return (string)GetValue(IconTextProperty); }
+            set { SetValue(IconTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconTextProperty =
+            DependencyProperty.Register("IconText", typeof(string), typeof(ECButton), new PropertyMetadata(string.Empty));
+    }
+}
